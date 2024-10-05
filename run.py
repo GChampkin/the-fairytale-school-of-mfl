55e2384654dae732d35996d14a3e5a07de5cc90a
Gregory Champkin <wlhpz57z@students.codeinstitute.net>
2024-10-05 12:50:33 +0000

    Add validation to check for exactly 11 values

--- run.py
+++ run.py
@@ -22,6 +22,21 @@ def get_year_10_data():
     print("Example: Bambi, 7, 20, 46, 32, 54, 76, 49, 59, 47, 60 \n")
 
     data_str = input("Enter your data here:\n")
-    print(f"The data provided is: {data_str}")
+    
+    assessment_data = data_str.split(",")
+    validate_data(assessment_data)
+
+
+def validate_data(values):
+    """
+    Inside the try, raises a ValueError message if number of values does not equal 11
+    """
+    try:
+        if len(values) != 11:
+            raise ValueError(
+                f"Exactly 11 values required. You entered {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invaid data: {e}. Please try again. \n")
 
 get_year_10_data()
